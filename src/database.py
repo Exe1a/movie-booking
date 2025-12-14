cf6cc6bd2b3093508d36bc3cab01de5a8f20ffc9
Exe1a <exe1a@yandex.ru>
2025-12-14 21:15:40 +0300

    relocate reseting db

--- src/database.py
+++ src/database.py
@@ -1,14 +1,4 @@
 from sqlalchemy import create_engine
 from dotenv import dotenv_values
-from fastapi import APIRouter
-from models import Base
-
-router = APIRouter(prefix="/db")
 
 engine = create_engine(dotenv_values(".env").get("DATABASE_URL"))
-
-@router.post("/reset")
-def db_reset():
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
-    return {"status": "Database was reset."}
---
+++ src/routers/database.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from src.database import engine
+from models import Base
+
+router = APIRouter(prefix="/db")
+
+@router.post("/reset")
+def db_reset():
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    return {"status": "Database was reset."}
