35b2c8b0d22b042b47f1bb74e20f138f8b02661e
exela <exe1a@yandex.ru>
2026-02-04 14:35:10 +0300

    fix in create_env

--- create_env.py
+++ create_env.py
@@ -1,5 +1,5 @@
 with open(".env", 'w') as file:
-    file.write('DATABASE_URL = "postgresql+psycopg://postgres:password@localhost:5432/postgres"')
-    file.write('SECRET_JWT_KEY = "SUPER_SECRET_JWT_KEY"')
-    file.write('DATABASE_RESET_KEY = "database_key"')
+    file.write('DATABASE_URL = "postgresql+psycopg://postgres:password@localhost:5432/postgres"\n')
+    file.write('SECRET_JWT_KEY = "SUPER_SECRET_JWT_KEY"\n')
+    file.write('DATABASE_RESET_KEY = "database_key"\n')
 print(f"File {file.name} was created")
